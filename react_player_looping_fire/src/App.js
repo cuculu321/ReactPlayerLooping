42237f177232c2825d750ea179a432410abefc4e
cuculu321 <matiyu321@gmail.com>
2023-07-17 11:06:35 +0900

    ループ時に毎回onprogressが発火する

--- react_player_looping_fire/src/App.js
+++ react_player_looping_fire/src/App.js
@@ -2,20 +2,25 @@ import React from 'react';
 import ReactPlayer from 'react-player'
 
 function App() {
-
   const [loopCount, setLoopCount] = React.useState(0)
+  const [duration, setDuration] = React.useState(0)
 
-  function onEned() {
-    console.log('onEnded callback')
+  function handlerOnProgress(state) {
+    if (state.playedSeconds > duration - 1.5) {
+      setLoopCount(loopCount + 1)
+    }
   }
 
   return (
     <div>
       <ReactPlayer
         url='https://samplelib.com/lib/preview/mp4/sample-5s.mp4' 
+        controls={true}
         playing={true}
-        loop={false}
-        onEnded={onEned}
+        loop={true}
+        onEnded={() => console.log('onEnded')} //悲しいかな発火しない子
+        onDuration={setDuration}
+        onProgress={handlerOnProgress}
         />
         <h3>
           Loop count: {loopCount}
